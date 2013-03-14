c363449c11cbb8992f8664dd4cf6b18a8d8b35b2
LaoLin <hi@laolin.com>
2013-03-15 03:04:43 +0800

    查询中提示和超时提示

--- www/apps/app.py.js
+++ www/apps/app.py.js
@@ -18,11 +18,28 @@ $(function(){
     url="http://api.laolin.com/rest/api/pinyin/list/"+
     'js=1&func=set_info_box_main'+
     '&q='+$('#'+id_q).attr('value');
-    laolin.fn.loadJs('pyscript',url);
+    py_waiting(6);
+    laolin.fn.loadJs('pyscript',url);//会调用 set_info_box_main(htm)
     return false;
   }
   
-  //这个函数是给跨域ajax文件生成js代码调用的
+  //这个函数是给 ajax文件生成js代码回调用的
   function set_info_box_main(str) {
     $('#info-box-main').html(str);
+     py_done();
+  }
+  
+  function py_waiting(n) {
+    if(n<=1) {
+      set_info_box_main('[error]网络超时。');
+      return;
+    }
+    n--;
+    $("#py_btn").attr('value','查询中('+n+')').attr('disabled',true);
+    laolin.data.py_timeid=setTimeout("py_waiting("+n+")",1000);
+
+  }
+  function py_done() {
+    $("#py_btn").attr('value','查询').attr('disabled',false);
+    clearTimeout(laolin.data.py_timeid);
   }
